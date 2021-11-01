be25051910defa47ef6402afd5ce1c8fcee8d188
marcsoh-smu <71428250+marcsoh-smu@users.noreply.github.com>
2021-11-01 22:57:17 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
-import flask, time
+import flask
+import time
 
 app = flask.Flask(__name__)
 
 
 @app.route("/")
 def index():
+<<<<<<< Updated upstream
     return "Welcome!!! ", time.localtime
+=======
+    return "Welcome!!sssss", time.localtime
+>>>>>>> Stashed changes
