d5e5662e056bed6185dfad6a3ae03d126f8596f6
marcsoh-smu <71428250+marcsoh-smu@users.noreply.github.com>
2021-11-01 22:58:06 +0800

    testing jenkins

--- src/app.py
+++ src/app.py
@@ -6,8 +6,4 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-<<<<<<< Updated upstream
     return "Welcome!!! ", time.localtime
-=======
-    return "Welcome!!sssss", time.localtime
->>>>>>> Stashed changes
