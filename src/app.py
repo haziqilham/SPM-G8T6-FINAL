a984370e27a32c098755a7854a8c7181e33c1bec
Jun Hong <jun_hong1997@hotmail.com>
2021-11-04 03:49:23 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,26 +1,21 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import flask, time
-=======
+
 import flask
 import time
->>>>>>> Claudia
-=======
+
 import flask
 import time
->>>>>>> Marcus
+
 
 app = flask.Flask(__name__)
 
 
 @app.route("/")
 def index():
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     return "Welcome!!! ",time.localtime
-=======
+
     return "PUSHED AT 11:40 ", time.localtime
->>>>>>> Claudia
-=======
+
     return "Welcome!!1!!!1:4aaaa1ss!!!sss", time.localtime
->>>>>>> Marcus
