8098acaf3890907454a5345c79bc4eeb5a60fc3f
marcsoh-smu <71428250+marcsoh-smu@users.noreply.github.com>
2021-11-11 10:06:00 +0800

    Update app.py
    change the import codes into different lines

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask import request
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import datetime as dt
